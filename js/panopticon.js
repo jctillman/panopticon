6ee05708ef402cc1741335d10fe5453d0bdfdb58
James Tillman <james.tillman.7@gmail.com>
2015-05-01 14:25:33 -0400

    Working now while refactored.

--- js/panopticon.js
+++ js/panopticon.js
@@ -6,17 +6,19 @@ var sizeOfLarger = function(arr, size) { return arr.filter(function(n){return n 
 
 function Panopticon(container, interval, options){
 
-	this.onFrames = [];
-
+	//Open up, and set basic settings.
+	//Alpha settings--those you can set without knowing what the resolution of the camera will be
+	//Beta settings get called when we first get data back from the camera.
+	
 	var self = this;
-	this.initialized = false;
-
-	//Basic settings--those you can set without knowing what the resolution of the camera will be.
+	this.onFrames = [];
 	this.setAlphaSettings(options);
 
 	var fh = new FrameHandler(container, interval, options);
 	if(this.mode == 1){
 		fh.onFrame(function(img){
+			if(!self.betaInitialized && !!img){ self.setBetaSettings(img);}
+			if(!self.resolutionRows || !self.resolutionCols){return;}
 			return self.perFrameFirstMode(img);
 		});
 	}else if(self.mode == 2){
@@ -30,27 +32,33 @@ Panopticon.prototype.onFrame = function(func){
 };
 
 Panopticon.prototype.setAlphaSettings = function(options){
-	//Basic settings
+
+	//Have no yet done beta initialization.
+	this.betaInitialized = false;
+
 	//How many columns do we want to examine?
 	this.numCols = options.numCols || 20;
 	this.numRows = options.numRows || 10;
-	//How many of these columns do we want to require to be moving, before anything moves?
+	
+	//How many of these columns do we want to require to be moving before anything moves.
 	this.requiredCols = options.requiredCols || 4;
 	this.requiredRows = options.requiredRows || 4;
+
+	//Use mode 1, by default.
 	this.mode = options.mode || 1;
 
 	//Slightly more advanced settings.
 	//What's the biggest movement we anticipate, in per-percentage-of-screen distances?  Won't really work past 20%, probably.
 	//This also takes up some serious time working.
 	this.biggestMovementPercentage = options.biggestMovementPercentage || 10;
-	this.dampening = options.dampening || 10;
+	this.dampening = options.dampening || 150;
 	this.stepSearchSize = options.stepSearchSize || 1;
-	this.minimumNoticedScrollDistance = options.minimumNoticedScrollDistance || 3;
+	this.minimumNoticedScrollDistance = options.minimumNoticedScrollDistance || 5;
 	this.showVideo = options.showVideo || false;
 };
 
 
-Panopticon.prototype.initializer = function(img){
+Panopticon.prototype.setBetaSettings = function(img){
 	if(img && img.width && img.height){
 		this.resolutionCols = Math.floor(img.width / this.numCols);
 		this.resolutionRows = Math.floor(img.height / this.numRows);
@@ -63,13 +71,6 @@ Panopticon.prototype.initializer = function(img){
 
 
 Panopticon.prototype.perFrameFirstMode = function(img){
-
-	
-	if(!this.initialized && !!img){ this.initializer(img);}
-	//console.log(img);
-	if(!this.resolutionRows || !this.resolutionCols){return;}
-
-	
 
 	//Reduce the resolution for both columns and for the rows.
 	var toCols = imgproc.cols(this.oldImg, img, this.resolutionCols);
@@ -93,20 +94,12 @@ Panopticon.prototype.perFrameFirstMode = function(img){
 		this.onFrames[x](obj);
 	}
 
-	//postprocessing needed for stuff.
-	//We need the old image, so we can product the next old cols.
-	//And we need the old cols, which is actually the difference we had for this time.
 	this.oldImg = imgproc.copy(img);
 	this.oldCols = deepCopy(toCols);
 	this.oldRows = deepCopy(toRows);
 
 	if (this.showVideo){return toReturn || img;}
 };
-
-
-
-
-
 
 
 
